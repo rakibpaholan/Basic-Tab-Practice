85d3773d14045bc9a685e598d71803be671b51b6
rakibpaholan <rakibpaholan9999@gmail.com>
2021-03-01 16:12:11 +0600

    Basic Tab Prctice

---
+++ app/src/main/java/com/example/tabspractice/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.tabspractice;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager)findViewById(R.id.viewPagerId);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        CustomAdapter customAdapter = new CustomAdapter(fragmentManager);
+        viewPager.setAdapter(customAdapter);
+
+    }
+
+
+   class CustomAdapter extends FragmentStatePagerAdapter{
+
+       public CustomAdapter(@NonNull FragmentManager fm) {
+           super(fm);
+       }
+
+       @NonNull
+       @Override
+       public Fragment getItem(int position) {
+           Fragment fragment = null;
+           if (position==0){
+               fragment = new FragmentOne();
+           }else if (position==1){
+               fragment = new FragmentTwo();
+           }else if (position==2){
+               fragment = new FragmentThree();
+           }
+           return fragment;
+       }
+
+       @Override
+       public int getCount() {
+           return 3;
+       }
+   }
+}
